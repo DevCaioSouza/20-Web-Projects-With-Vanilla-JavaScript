e2647ef2f30965ec8e4ae1c971fd2dcf1947bfea
caio souza <caiocbahia@hotmail.com>
2022-05-23 17:48:29 -0300

    Check required & refactor

--- script.js
+++ script.js
@@ -8,50 +8,44 @@ const password2 = document.getElementById("password2");
 function showError(input, message) {
   const formControl = input.parentElement;
   formControl.className = "form-control error";
-  const small = formControl.querySelector('small');
-  small.innerText = message
+  const small = formControl.querySelector("small");
+  small.innerText = message;
 }
 
 //Show success outline
 
-function showSuccess(input){
+function showSuccess(input) {
   const formControl = input.parentElement;
   formControl.className = "form-control success";
 }
 
 //check email is valid
-function isValidEmail(email){
-  const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-  return re.test(String(email).toLowerCase())
+function isValidEmail(email) {
+  const re =
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+  return re.test(String(email).toLowerCase());
+}
+
+//Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input){
+    if(input.value.trim() === ''){
+      showError(input, `${getFieldName(input)} is required`)
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+//Get fieldname
+
+function getFieldName(input){
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
 }
 
 //Event Listeners
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if (username.value === "") {
-    showError(username, "Username is required");
-  } else {
-    showSuccess(username);
-  }
-   
-  if (email.value === "") {
-    showError(email, "Email is required");
-  } else if (!isValidEmail(email.value)){
-    showError(email, 'Email is not valid')
-  } else {
-    showSuccess(email);
-  }
-   
-  if (password.value === "") {
-    showError(password, "Password is required");
-  } else {
-    showSuccess(password);
-  }
-   
-  if (password2.value === "") {
-    showError(password2, "Password confirmation is required");
-  } else {
-    showSuccess(password2);
-  }
-   
+
+  checkRequired([username, email, password, password2])
 });
