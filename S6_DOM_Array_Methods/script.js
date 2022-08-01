3eb8da8630d53440ef8ffe03aba2274b410b26b6
unknown <caiocbahia@hotmail.com>
2022-08-01 00:37:59 -0300

    added function doubleMoney on click

--- S6_DOM_Array_Methods/script.js
+++ S6_DOM_Array_Methods/script.js
@@ -29,6 +29,15 @@ async function getRandomUser() {
   addData(newUser)
 }
 
+//Double User Money
+function doubleMoney (user){
+  data = data.map(user => {
+    return { ...user, money: user.money * 2 }
+  })
+
+  updateDOM()
+}
+
 // Add new obj to data array
 
 function addData(obj) {
@@ -59,3 +68,4 @@ function formatMoney(number){
 
 // Event listeners
 addUserBtn.addEventListener('click', getRandomUser)
+doubleBtn.addEventListener('click', doubleMoney)
